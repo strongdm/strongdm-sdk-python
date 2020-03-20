a30bf2bc08bd61ba0d960633be4702bbaf90a134
Ulderico Cirello <uldericofilho@gmail.com>
2020-03-20 15:21:21 -0700

    bump python version
    Addresses #5512
    
    GitOrigin-RevId: 7d24d1cbd73d55abed5571410b6b1a675378cf1c

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ from distutils.core import setup
 setup(
     name='strongdm',
     packages=['strongdm'],
-    version='1.0.0',
+    version='1.0.1',
     license='apache-2.0',
     description='strongDM SDK for the Python programming language.',
     author='strongDM Team',
     author_email='sdk-feedback@strongdm.com',
     url='https://github.com/strongdm/strongdm-sdk-python',
-    download_url='https://github.com/user/reponame/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/user/reponame/archive/v1.0.1.tar.gz',
     keywords=[
         'strongDM', 'sdm', 'api', 'automation', 'security', 'audit',
         'database', 'server', 'ssh', 'rdp'
