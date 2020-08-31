6046693cd4e2e5f832538dbea6b13d7298aba34a
Alex Okafor <alex@strongdm.com>
2020-08-31 12:27:30 -0400

    python: regenerate python SDK
    GitOrigin-RevId: 65a231beed262725924e4df0682f33ad17295904

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from distutils.core import setup
 setup(
     name='strongdm',
     packages=['strongdm'],
-    version='1.0.12',
+    version='1.0.13',
     license='apache-2.0',
     description='strongDM SDK for the Python programming language.',
     long_description=long_description,
@@ -32,7 +32,7 @@ setup(
     author_email='sdk-feedback@strongdm.com',
     url='https://github.com/strongdm/strongdm-sdk-python',
     download_url=
-    'https://github.com/strongdm/strongdm-sdk-python/archive/v1.0.12.tar.gz',
+    'https://github.com/strongdm/strongdm-sdk-python/archive/v1.0.13.tar.gz',
     keywords=[
         'strongDM', 'sdm', 'api', 'automation', 'security', 'audit',
         'database', 'server', 'ssh', 'rdp'
