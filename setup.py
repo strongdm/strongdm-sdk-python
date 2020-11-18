e33768784f2f9ede617394643e704fdefffacfd5
Evan Todd <evan@etodd.io>
2020-11-18 14:52:59 -0800

    use setuptools instead of distutils
    setuptools is apparently the new standard.
    
    GitOrigin-RevId: 0b14c9ac52b0fb2319e58a56e5d4edacdd5de6cd

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-from distutils.core import setup
+from setuptools import setup
 setup(
     name='strongdm',
     packages=['strongdm'],
