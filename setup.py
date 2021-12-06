5e1fb10e8a0f43abb89e46abae1cc543949c8136
Evan Todd <evan@etodd.io>
2021-12-06 15:37:39 -0800

    upgrade grpcio from 1.33.2 to 1.42.0
    GitOrigin-RevId: 92971a28298b3583e87055fadee7b28aba40323e

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 setup(
     name='strongdm',
     packages=['strongdm'],
-    version='1.0.31',
+    version='1.0.32',
     license='apache-2.0',
     description='strongDM SDK for the Python programming language.',
     long_description=long_description,
@@ -32,13 +32,13 @@ setup(
     author_email='sdk-feedback@strongdm.com',
     url='https://github.com/strongdm/strongdm-sdk-python',
     download_url=
-    'https://github.com/strongdm/strongdm-sdk-python/archive/v1.0.31.tar.gz',
+    'https://github.com/strongdm/strongdm-sdk-python/archive/v1.0.32.tar.gz',
     keywords=[
         'strongDM', 'sdm', 'api', 'automation', 'security', 'audit',
         'database', 'server', 'ssh', 'rdp'
     ],
     install_requires=[
-        'grpcio==1.33.2',
+        'grpcio==1.42.0',
         'googleapis-common-protos==1.52.0',
     ],
     classifiers=[
