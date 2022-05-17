0f04f093bb917823844f41ab1b1a118e80985217
Ulderico Cirello <uldericofilho@gmail.com>
2022-05-17 14:44:02 -0700

    python: regenerate SDK after version bump
    GitOrigin-RevId: e011e00c0ba99ef5f2fbb6533d95c0c4deb26832

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 setup(
     name='strongdm',
     packages=['strongdm'],
-    version='2.2.0',
+    version='2.3.0',
     license='apache-2.0',
     description='strongDM SDK for the Python programming language.',
     long_description=long_description,
@@ -34,7 +34,7 @@ setup(
     author_email='sdk-feedback@strongdm.com',
     url='https://github.com/strongdm/strongdm-sdk-python',
     download_url=
-    'https://github.com/strongdm/strongdm-sdk-python/archive/v2.2.0.tar.gz',
+    'https://github.com/strongdm/strongdm-sdk-python/archive/v2.3.0.tar.gz',
     keywords=[
         'strongDM', 'sdm', 'api', 'automation', 'security', 'audit',
         'database', 'server', 'ssh', 'rdp'
